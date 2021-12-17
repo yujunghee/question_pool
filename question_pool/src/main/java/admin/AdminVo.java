8bedb761d4bd677fbbab5dfa6072be4453c27f7c
taehyunNam <nth2660@naver.com>
2021-12-17 14:22:27 +0900

    adminvo 추가

--- question_pool/src/main/java/admin/AdminVo.java
+++ question_pool/src/main/java/admin/AdminVo.java
@@ -1,8 +1,18 @@
 package admin;
 
-public class AdminVo {
+import java.sql.Timestamp;
 
-	private int amdinno;
-	private String email;
-	private String pwd;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class AdminVo {
+	
+	private int admin_no;
+	private String admin_email;
+	private String admin_pwd;
+	private String admin_name;
+	private Timestamp admin_regdate;
+			 
 }
