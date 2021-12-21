0596e0f3dc5fb60b617d5b049af4b562999edcdd
taehyunNam <nth2660@naver.com>
2021-12-21 16:59:21 +0900

    schoolinsert (진행중)

--- question_pool/src/main/java/question/QuestionController.java
+++ question_pool/src/main/java/question/QuestionController.java
@@ -9,36 +9,49 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class QuestionController {
-	
+
 	@Autowired
 	QuestionService questionService;
-	
+
 	@GetMapping("/admin/question/pool.do")
 	public String pool() {
-		return "admin/question/pool"; //문제등록(학교/연도/회차선택창으로 이동)
+		return "admin/question/pool"; // 문제등록(학교/연도/회차선택창으로 이동)
 	}
-	
+
 	@GetMapping("/admin/question/input.do")
 	public String input() {
 		return "admin/question/input";
 	}
-	
+
 	@RequestMapping("/admin/question/insert.do")
 	public String insert(QuestionVo qv, ExampleVo ev, HttpServletRequest req) {
-		
+
 		int r = questionService.insert(qv, ev);
-		
-		if(r > 1) {
+
+		if (r > 1) {
 			req.setAttribute("msg", "정상적으로 등록되었습니다.");
 			req.setAttribute("url", "/question_pool/admin/question/input.do");
 		} else {
-			req.setAttribute("msg", "등록 오류"); 
+			req.setAttribute("msg", "등록 오류");
 		}
 		return "admin/include/return";
 	}
-	
+
 	@GetMapping("/admin/question/school.do")
 	public String school() {
-		return "admin/question/school"; //학교등록페이지로 이동
+		return "admin/question/school"; // 학교등록페이지로 이동
 	}
+
+//	@RequestMapping("/admin/question/insertschool.do")
+//	public String insertschool(QuestionVo qv, ExampleVo ev, HttpServletRequest req) {
+//		int r = questionService.insert(qv, ev);
+//
+//		if (r > 1) {
+//			req.setAttribute("msg", "정상적으로 등록되었습니다.");
+//			req.setAttribute("url", "/question_pool/admin/question/school.do");
+//		} else {
+//			req.setAttribute("msg", "등록 오류");
+//		}
+//		return "admin/include/return";
+//	}
 }
