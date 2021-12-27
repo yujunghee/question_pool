fb1b88f98b043c81d44cb40582df032307fa8211
taehyunNam <nth2660@naver.com>
2021-12-27 19:00:16 +0900

    악몽

--- question_pool/src/main/java/question/QuestionService.java
+++ question_pool/src/main/java/question/QuestionService.java
@@ -10,11 +10,12 @@ public interface QuestionService {
 	int insertQuestion(QuestionVo qv);
 	int insertExample(ExampleVo ev);
 	List<QuestionVo> selectQuestionlist(QuestionVo qv);
+	List<QuestionVo> selectyear(QuestionVo qv);
+	List<QuestionVo> selectsemester(QuestionVo qv);
 	List<ExampleVo> selectExamplelist(ExampleVo ev);
 	int insertSchool(SchoolVo sv);
 
 	int insertExam(QuestionVo qv);
 	List<QuestionVo> selectexam(QuestionVo qv);
-	QuestionVo year(int year);
 	
 }
